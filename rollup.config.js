5c817a1a85b0c19529ede8f7bf01542e3dc98b50
Stanislav Lesnikov <stan@nashdot.com>
2016-02-18 23:52:41 +0100

    Remove quotes from properties

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  'entry': 'lib/index.js',
-  'sourceMap': true,
-  'plugins': [
+  entry: 'lib/index.js',
+  sourceMap: true,
+  plugins: [
     babel()
   ]
 };
