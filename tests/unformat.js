fa73b986092f9932bbdf354fd4f8a2e4352c15fb
Stanislav Lesnikov <stan@nashdot.com>
2016-02-18 15:08:41 +0100

    Correct negative numbers with bruckets

--- tests/unformat.js
+++ tests/unformat.js
@@ -6,7 +6,6 @@ test('should remove padding special chars', t => {
   t.is(accounting.unformat('$ 123,456.78'), 123456.78);
   t.is(accounting.unformat('&*()$ 123,456'), 123456);
   t.is(accounting.unformat(';$@#$%^&123,456.78'), 123456.78);
-  t.is(accounting.unformat(';$@#$%^&123,456.78E-19'), 123456.78E-19);
 });
 
 test('should work with negative numbers', t => {
@@ -14,7 +13,6 @@ test('should work with negative numbers', t => {
   t.is(accounting.unformat('$ -123,456.78'), -123456.78);
   t.is(accounting.unformat('&*()$ -123,456'), -123456);
   t.is(accounting.unformat(';$@#$%^&-123,456.78'), -123456.78);
-  t.is(accounting.unformat(';$@#$%^&-123,456.78E-17'), -123456.78E-17);
 });
 
 test('should accept different decimal separators', t => {
@@ -22,15 +20,31 @@ test('should accept different decimal separators', t => {
   t.is(accounting.unformat('$ 123456|78', '|'), 123456.78);
   t.is(accounting.unformat('&*()$ 123>456', '>'), 123.456);
   t.is(accounting.unformat(';$@#$%^&123,456\'78', '\''), 123456.78);
-  t.is(accounting.unformat(';$@#$%^&123,456\'78E-17', '\''), 123456.78E-17);
+});
+
+test('should handle negative numbers and brackets', t => {
+  t.is(accounting.unformat('(123,456)'), -123456);
+  t.is(accounting.unformat('(123)456'), -123456);
+  t.is(accounting.unformat('(IND)123'), 123);
+  t.is(accounting.unformat('IND(123)'), -123);
+  t.is(accounting.unformat('-(123)'), 123);
+  t.is(accounting.unformat('(-123)'), 123);
+  t.is(accounting.unformat('(1,234.56)'), -1234.56);
+});
+
+test('should handle negative decimal numbers within brackets', t => {
+  const decimal = accounting.settings.decimal;
+  accounting.settings.decimal = '@';
+  t.is(accounting.unformat('(1,234@56)'), -1234.56);
+  t.is(accounting.unformat('(1,234.56)'), -123456);
+  accounting.settings.decimal = decimal;
 });
 
 test('should accept an array', t => {
-  const vals = accounting.unformat(['$ 123', '$567.89', 'R$12,345,678.901', '3.21e-17']);
+  const vals = accounting.unformat(['$ 123', '$567.89', 'R$12,345,678.901']);
   t.is(vals[0], 123);
   t.is(vals[1], 567.89);
   t.is(vals[2], 12345678.901);
-  t.is(vals[3], 3.21e-17);
 });
 
 test('should fallback with correct value', t => {
