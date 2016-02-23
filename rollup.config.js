9a3c7611ad7c6404921027e8d954ffe0504a8180
Stanislav Lesnikov <stan@nashdot.com>
2016-02-23 09:35:58 +0100

    Use object-assign polyfill

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,22 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'lib/index.js',
   sourceMap: true,
   plugins: [
-    babel()
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      extensions: ['.js']
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
   ]
 };
