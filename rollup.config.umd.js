6f5083c6aaf57dd3345b7a178327c9ebff232512
Stanislav Lesnikov <stan@nashdot.com>
2016-02-25 04:17:57 +0100

    Make happy old npm

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -1,7 +1,8 @@
+/* eslint no-var: 0 */
 import { readFileSync } from 'fs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
-const pkg = JSON.parse(readFileSync('package.json', 'utf-8'));
+var pkg = JSON.parse(readFileSync('package.json', 'utf-8'));
 import config from './rollup.config';
 
 config.plugins.unshift(nodeResolve({
