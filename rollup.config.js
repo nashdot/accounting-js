523469b416b31f73f56d25fceddd4300b75ec303
Stanislav Lesnikov <stan@nashdot.com>
2016-02-25 04:09:39 +0100

    Make happy old npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
+/* eslint no-var: 0 */
 import { readFileSync } from 'fs';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 
-const pkg = JSON.parse(readFileSync('package.json', 'utf-8'));
+var pkg = JSON.parse(readFileSync('package.json', 'utf-8'));
 
 export default {
   entry: pkg['jsnext:main'],
