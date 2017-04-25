af4715d6c1911725d299c5f50ac62295562933c2
sungmin kim <ignocide@gmail.com>
2017-04-25 11:24:13 +0900

    Add choice of rounding method for precision

--- tests/toFixed.js
+++ tests/toFixed.js
@@ -9,4 +9,6 @@ test('should round', t => {
   t.is(accounting.toFixed(0.615, 2), '0.62');
   t.is(accounting.toFixed(74.725, 2), '74.73');
   t.is(accounting.toFixed(158.605, 2), '158.61');
+  t.is(accounting.toFixed(3.1415, 2, 1), '3.15');
+  t.is(accounting.toFixed(12.56, 1, -1), '12.5');
 });
