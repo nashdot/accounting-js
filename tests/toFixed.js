48978202620a090b3045064cfb13b855c0b0cae2
Stanislav Lesnikov <stan@nashdot.com>
2017-07-02 15:37:09 +0200

    Update libs

--- tests/toFixed.js
+++ tests/toFixed.js
@@ -1,12 +1,12 @@
 import test from 'ava';
-import accounting from '..';
+import { toFixed } from '..';
 
 test('performs basic float zero-padding', t => {
-  t.is(accounting.toFixed(54321, 5), '54321.00000');
+  t.is(toFixed(54321, 5), '54321.00000');
 });
 
 test('should round', t => {
-  t.is(accounting.toFixed(0.615, 2), '0.62');
-  t.is(accounting.toFixed(74.725, 2), '74.73');
-  t.is(accounting.toFixed(158.605, 2), '158.61');
+  t.is(toFixed(0.615, 2), '0.62');
+  t.is(toFixed(74.725, 2), '74.73');
+  t.is(toFixed(158.605, 2), '158.61');
 });
--- tests/unformat.js
+++ tests/unformat.js
@@ -1,55 +1,55 @@
 import test from 'ava';
-import accounting from '..';
+import { settings, unformat } from '..';
 
 test('should remove padding special chars', t => {
-  t.is(accounting.unformat('$ 123,456'), 123456);
-  t.is(accounting.unformat('$ 123,456.78'), 123456.78);
-  t.is(accounting.unformat('&*()$ 123,456'), 123456);
-  t.is(accounting.unformat(';$@#$%^&123,456.78'), 123456.78);
+  t.is(unformat('$ 123,456'), 123456);
+  t.is(unformat('$ 123,456.78'), 123456.78);
+  t.is(unformat('&*()$ 123,456'), 123456);
+  t.is(unformat(';$@#$%^&123,456.78'), 123456.78);
 });
 
 test('should work with negative numbers', t => {
-  t.is(accounting.unformat('$ -123,456'), -123456);
-  t.is(accounting.unformat('$ -123,456.78'), -123456.78);
-  t.is(accounting.unformat('&*()$ -123,456'), -123456);
-  t.is(accounting.unformat(';$@#$%^&-123,456.78'), -123456.78);
+  t.is(unformat('$ -123,456'), -123456);
+  t.is(unformat('$ -123,456.78'), -123456.78);
+  t.is(unformat('&*()$ -123,456'), -123456);
+  t.is(unformat(';$@#$%^&-123,456.78'), -123456.78);
 });
 
 test('should accept different decimal separators', t => {
-  t.is(accounting.unformat('$ 123,456', ','), 123.456);
-  t.is(accounting.unformat('$ 123456|78', '|'), 123456.78);
-  t.is(accounting.unformat('&*()$ 123>456', '>'), 123.456);
-  t.is(accounting.unformat(';$@#$%^&123,456\'78', '\''), 123456.78);
+  t.is(unformat('$ 123,456', ','), 123.456);
+  t.is(unformat('$ 123456|78', '|'), 123456.78);
+  t.is(unformat('&*()$ 123>456', '>'), 123.456);
+  t.is(unformat(';$@#$%^&123,456\'78', '\''), 123456.78);
 });
 
 test('should handle negative numbers and brackets', t => {
-  t.is(accounting.unformat('(123,456)'), -123456);
-  t.is(accounting.unformat('(123)456'), -123456);
-  t.is(accounting.unformat('(IND)123'), 123);
-  t.is(accounting.unformat('IND(123)'), -123);
-  t.is(accounting.unformat('-(123)'), 123);
-  t.is(accounting.unformat('(-123)'), 123);
-  t.is(accounting.unformat('(1,234.56)'), -1234.56);
+  t.is(unformat('(123,456)'), -123456);
+  t.is(unformat('(123)456'), -123456);
+  t.is(unformat('(IND)123'), 123);
+  t.is(unformat('IND(123)'), -123);
+  t.is(unformat('-(123)'), 123);
+  t.is(unformat('(-123)'), 123);
+  t.is(unformat('(1,234.56)'), -1234.56);
 });
 
 test('should handle negative decimal numbers within brackets', t => {
-  const decimal = accounting.settings.decimal;
-  accounting.settings.decimal = '@';
-  t.is(accounting.unformat('(1,234@56)'), -1234.56);
-  t.is(accounting.unformat('(1,234.56)'), -123456);
-  accounting.settings.decimal = decimal;
+  const decimal = settings.decimal;
+  settings.decimal = '@';
+  t.is(unformat('(1,234@56)'), -1234.56);
+  t.is(unformat('(1,234.56)'), -123456);
+  settings.decimal = decimal;
 });
 
 test('should accept an array', t => {
-  const vals = accounting.unformat(['$ 123', '$567.89', 'R$12,345,678.901']);
+  const vals = unformat(['$ 123', '$567.89', 'R$12,345,678.901']);
   t.is(vals[0], 123);
   t.is(vals[1], 567.89);
   t.is(vals[2], 12345678.901);
 });
 
 test('should fallback with correct value', t => {
-  t.is(accounting.unformat('string'), 0);
-  t.is(accounting.unformat({ joss: 1 }), 0);
-  t.is(accounting.unformat('string', ',', null), null);
-  t.is(accounting.unformat('string', ',', 'failed'), 'failed');
+  t.is(unformat('string'), 0);
+  t.is(unformat({ joss: 1 }), 0);
+  t.is(unformat('string', ',', null), null);
+  t.is(unformat('string', ',', 'failed'), 'failed');
 });
