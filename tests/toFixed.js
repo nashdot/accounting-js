47173401dd487d112b1910d4940bc948875abd86
Stanislav Lesnikov <stan@nashdot.com>
2017-07-02 16:41:09 +0200

    Update toFixed.js

--- tests/toFixed.js
+++ tests/toFixed.js
@@ -6,9 +6,9 @@ test('performs basic float zero-padding', t => {
 });
 
 test('should round', t => {
-  t.is(accounting.toFixed(0.615, 2), '0.62');
-  t.is(accounting.toFixed(74.725, 2), '74.73');
-  t.is(accounting.toFixed(158.605, 2), '158.61');
-  t.is(accounting.toFixed(3.1415, 2, 1), '3.15');
-  t.is(accounting.toFixed(12.56, 1, -1), '12.5');
+  t.is(toFixed(0.615, 2), '0.62');
+  t.is(toFixed(74.725, 2), '74.73');
+  t.is(toFixed(158.605, 2), '158.61');
+  t.is(toFixed(3.1415, 2, 1), '3.15');
+  t.is(toFixed(12.56, 1, -1), '12.5');
 });
