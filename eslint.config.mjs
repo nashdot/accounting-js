968ab8a28705d2714426450aae5a006d4f963b0a
Stanislav LESNIKOV <stan@nashdot.com>
2025-06-05 00:00:13 +0200

    Add eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    files: ['**/*.{js, ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+  },
+  {
+    name: 'stylistic',
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/comma-dangle': ['error', {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'never',
+        'importAttributes': 'always-multiline',
+        'dynamicImports': 'always-multiline',
+        'enums': 'always-multiline',
+        'generics': 'always-multiline',
+        'tuples': 'always-multiline',
+      }],
+      '@stylistic/indent': ['error', 2, { 'ignoredNodes': ['ConditionalExpression'] }],
+    },
+  }
+);
