b5fa783fb20203872e03771e36f8dcd9e8528f22
Stanislav LESNIKOV <stan@nashdot.com>
2025-06-05 12:31:11 +0200

    Format tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    format: ['cjs', 'esm'],
-    entry: ['./src/index.ts'],
-    dts: true,
-    shims: true,
-    skipNodeModulesBundle: true,
-    clean: true,
+  format: ['cjs', 'esm'],
+  entry: ['./src/index.ts'],
+  dts: true,
+  shims: true,
+  skipNodeModulesBundle: true,
+  clean: true,
 });
